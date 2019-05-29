d2a075729dfca181927f4ec689021b55ef14119e
Piotr Ornat <ornat.piotr@gmail.com>
2019-05-29 09:34:13 +0200

    Dodany panel opinii + zmiana w Connection Stringu dla publikacji

---
+++ Komis/Models/Opinia.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace Komis.Models
+{
+    public class Opinia
+    {
+        [BindNever]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
+        [StringLength(100, ErrorMessage = "Nazwa użytkownika jest za długa")]
+        public string NazwaUzytkownika { get; set; }
+
+        [Required(ErrorMessage = "Adres email jest wymagany")]
+        [StringLength(100, ErrorMessage = "Adres email jest za długi")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Wiadomość jest wymagana")]
+        [StringLength(5000, ErrorMessage = "Wiadomość jest za długa")]
+        public string Wiadomosc { get; set; }
+        public bool OczekujeOdpowiedzi { get; set; }
+    }
+}
